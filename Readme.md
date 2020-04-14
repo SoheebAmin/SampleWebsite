c5edad07f489d0690e6e826fe10cb9559382027d
Soheeb Amin <soheeb@gmail.com>
2020-04-14 14:49:52 +0300

    got js on js.html up and running

--- Readme.md
+++ Readme.md
@@ -2,3 +2,12 @@
 
 This project was made first and foremost to help myself practice creating all the elements of a dynamic website. The files in this folder create a sample website with the sole purpose of highlighting different features of HTML, CSS, Javascript.
 
+## Organization
+
+If it isn't clear already, the root folder has the HTML pages, with the CSS, JS, and other files having their own folders.
+
+## Some Things to Note
+
+The index page has the most explanation of tags and elements, with unique features of other pages explained on that page.
+
+The website relies on CSS from Bootstrap and W3 Schools, with their stylesheets included on the pages they are used. There are also local stylesheets, which the CSS of may be from antother website (mentioned in a comment on top) or it may be a self-made class to apply to select elements as needed.
--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,42 @@
-// This file is linked into by the index.html page to run any javascript needed by the web application.
+// This file needs to be linked as the source scripts page for JS, and then functions can be referenced here.
 
-// Automatic Slideshow - change image every 3 seconds
-
-var myIndex = 0;
-carousel();
-function carousel() {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
+function scramble()
+{
+  //grabs the input from the form
+  let text_to_scramble = document.querySelector('#scramble_text').value
+  
+  function getRandomInt(n) 
+  {
+    return Math.floor(Math.random() * n);
   }
-  myIndex++;
-  if (myIndex > x.length) {myIndex = 1}
-  x[myIndex-1].style.display = "block";
-  setTimeout(carousel, 3000);
-}
+  
+  function shuffle(s) 
+  {
+    // Convert String to array
+    var arr = s.split('');           
+    
+     // Length of the array
+    var n = arr.length;             
+    
+    for(var i=0 ; i<n-1 ; ++i) 
+    {
+      // Get random of [0, n-1]
+      var j = getRandomInt(n);   
 
+      // Swap arr[i] and arr[j]
+      var temp = arr[i];             
+      arr[i] = arr[j];
+      arr[j] = temp;
+    }
+    
+    // Convert Array to string and return results
+    s = arr.join('');               
+    return s;
+  }
+
+  //calls function above to scamble text
+  scrambled_text = shuffle(text_to_scramble);
+
+  // Alert to display
+  alert('Scambled Text: ' + scrambled_text);
+}

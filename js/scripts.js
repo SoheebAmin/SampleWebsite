73a116c70a74f1d92b4d438448dcab053818debf
Soheeb Amin <soheeb@gmail.com>
2020-04-15 15:24:22 +0300

    modals first steps to integration

--- js/scripts.js
+++ js/scripts.js
@@ -46,7 +46,21 @@ function scramble()
   alert('Scambled Text: ' + scrambled_text);
 }
 
-function changeStyle(url) 
+
+// Changes the CSS styles by modifying the name of the ref stylesheet with a counter
+let style_counter = 0;
+let current_stylesheets = 5;
+
+function changeStyle() 
 {
-  document.getElementById('stylesheet').href = url;
+  if (style_counter != current_stylesheets)
+  {
+    style_counter++;
+  }
+  else
+  {
+    style_counter = 0;
+  }
+  document.getElementById('stylesheet').href = "css/example_bgs_css/stylesheet" + style_counter +".css";
 }
+

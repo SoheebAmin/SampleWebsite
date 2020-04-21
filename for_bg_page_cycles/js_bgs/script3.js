7f99b0f7939c25d9734e80b04e2e51949af65912
Soheeb Amin <soheeb@gmail.com>
2020-04-22 00:44:01 +0300

    2 JS bgs + kill switches complete

---
+++ for_bg_page_cycles/js_bgs/script3.js
@@ -0,0 +1,12 @@
+// There's no way to unload Javascript without a refresh. This is super bad practice, but the only way I know to pull off
+// this page as intended. The variables that make the script work are killed using the updated script number.
+
+SENSITIVITY = 0;
+SIBLINGS_LIMIT = 0;
+DENSITY = 0;
+NODES_QTY = 0;
+ANCHOR_LENGTH = 0;
+MOUSE_RADIUS = 0;
+
+// Source:
+

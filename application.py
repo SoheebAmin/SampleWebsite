7b2cf4ae97c9d2223fd27cfbdee4a1aa1fd624e1
Soheeb Amin <soheeb@gmail.com>
2020-04-28 16:38:14 +0300

    created Flask page

--- application.py
+++ application.py
@@ -10,19 +10,24 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    var = "This Text is from Flask."
-    return render_template("index.html", var = var)
+    return render_template("index.html")
 
 @app.route("/bg_page.html")
 def bg_page():
     return render_template("bg_page.html")
 
+@app.route("/js_page.html")
+def js_page():
+    return render_template("js_page.html")
+
 @app.route("/forms.html")
 def forms():
     return render_template("forms.html")
 
-@app.route("/js_page.html")
-def js_page():
-    return render_template("js_page.html")
+@app.route("/flask_page.html")
+def flask_page():
+    return render_template("flask_page.html")
+
+
 
 
